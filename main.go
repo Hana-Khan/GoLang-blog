33e645bb0ceedd7efe88386272815dbba2cf82b0
Hina Khan <47082795+Hana-Khan@users.noreply.github.com>
2023-05-30 16:33:31 +0500

    end points added

--- main.go
+++ main.go
@@ -2,8 +2,8 @@
 package main
 
 import (
-    
-
+	"golang-blog/controllers"
+	"golang-blog/models"
     "github.com/gin-gonic/gin"
 )
 
@@ -12,7 +12,10 @@ func main() {
 
     models.ConnectDatabase()  // new!
 
-    // ...
-
+	router.POST("/posts", controllers.CreatePost)  // here!
+    router.GET("/posts", controllers.FindPosts)
+	router.GET("/posts/:id", controllers.FindPost)  // here!
+	router.PATCH("/posts/:id", controllers.UpdatePost)
+	router.DELETE("/posts/:id", controllers.DeletePost)
     router.Run("localhost:8080")
 }
